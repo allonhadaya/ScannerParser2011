37436ca2b5151e6fdf574dba8bc87c63293a89be
Allon Hadaya <allonhadaya@hotmail.com>
2011-11-19 17:10:28 -0500

    initial

---
+++ src/ScannerDemo.java
@@ -0,0 +1,20 @@
+/**
+ * @author Christelle
+ * 
+ */
+public class ScannerDemo {
+
+	private static String file1 = "testjay.txt";
+	private static int counter = 1;
+
+	public static void main(String args[]) {
+
+		TokenStream ts = new TokenStream(file1);
+
+		System.out.println(file1);
+
+		while (!ts.isEndofFile()) {
+			// TO BE COMPLETED
+		}
+	}
+}
